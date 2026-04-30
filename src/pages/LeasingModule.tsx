8b0e3c10c8e2545c24b7b00de02c1117da2c8e1f
SoumyaSriMishra <richsumi99@gmail.com>
2026-04-30 13:09:32 +0530

    Final polish, media cleanup, and functional renaming

--- src/pages/LeasingModule.tsx
+++
@@ -1,7 +0,0 @@
-export default function LeasingModule() {
-  return (
-    <div className="h-screen bg-charcoal flex items-center justify-center">
-      <h1 className="font-display text-4xl text-ivory font-light">Leasing Module</h1>
-    </div>
-  );
-}
--- src/pages/VenueModule.tsx
+++
@@ -1,7 +0,0 @@
-export default function VenueModule() {
-  return (
-    <div className="h-screen bg-charcoal flex items-center justify-center">
-      <h1 className="font-display text-4xl text-ivory font-light">Venue Module</h1>
-    </div>
-  );
-}
